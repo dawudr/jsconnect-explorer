15ca991dcf80f1c9882c9d4b8508fcc78fa823cd
Dawud Rahman <dawudr@gmail.com>
2017-11-17 12:43:50 +0000

    Add Navigation and Create Pipeline pages for initial demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,8 @@
-import {Component} from '@angular/core';
-
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs/Observable';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // tslint:disable-next-line
+  selector: 'body',
+  template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
-  title = 'JSConnect Explorer';
-  description = 'JSConnect Web console';
-
-//  constructor() {}
-
-//  items: Observable<any[]>;
-//  constructor(db: AngularFireDatabase) {
-//    this.items = db.list('/pipelines').valueChanges();
-//  }
-
-}
+export class AppComponent { }
